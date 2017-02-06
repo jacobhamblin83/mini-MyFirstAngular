784a1fb9763411889dbcbb459d35a8ae9a084f2f
Jacob Hamblin <jacobhamblin83@gmail.com>
2017-02-06 13:30:03 -0700

    friends are added

---
+++ js/mainCtrl.js
@@ -0,0 +1,11 @@
+angular.module('friendsList').controller('mainCtrl', function($scope){
+
+    $scope.name = "Jacob";
+
+    $scope.friends = [
+        "Steven",
+        "Jeremy",
+        "Dickface",
+    ]
+
+});
